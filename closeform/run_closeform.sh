9f76792438e45fddc3ac8853b71c1400fdd8e11c
Sufeng Niu <sniu@g.clemson.edu>
2016-11-18 12:11:05 -0500

    update the shell to fix small bug

--- closeform/run_closeform.sh
+++ closeform/run_closeform.sh
@@ -6,13 +6,11 @@ iter=30
 alpha=0.85
 L=7
 
-touch $data.emb
-
 echo "==========================================================================================="
 echo "configuration: representation dimension: $size, alpha: $alpha, length: $L, SVD iterations: $iter, using matrix inversion"
-python cf.py -s $size -a $alpha -l $L -t $iter -i
+python cf.py -s $size -a $alpha -l $L -t $iter -i -m $data.mat
 echo "==========================================================================================="
 echo "configuration: representation dimension: $size, alpha: $alpha, length: $L, SVD iterations: $iter, without using matrix inversion"
-python cf.py -s$size -a $alpha -l $L -t $iter
+python cf.py -s $size -a $alpha -l $L -t $iter -f -m $data.mat
 
 
